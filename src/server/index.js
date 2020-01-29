9efd05a808b6688913ad9e0b9a70b771fe682a17
Belinda Caylor <belinda.caylor@gmail.com>
2020-01-29 11:06:37 -0600

    feat: Install Aylien_textapi

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+let aylienData = [];
+
+//Setup Aylient API
+var AYLIENTextAPI = require('aylien_textapi');
+var textapi = new AYLIENTextAPI({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
 
 //Body Parser
 const bodyParser = require('body-parser');
